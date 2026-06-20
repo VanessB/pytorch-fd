Fixed incorrect broadcasting in InfoNCELoss.

source/python/torchfd/loss/nce.py
@@ -17,8 +17,20 @@ class InfoNCELoss(BaseVariationalBoundLoss):
 
     is_lower_bound = True
     
-    def __init__(self):
+    def __init__(self, sum_dim: int=0):
+        """
+        Create an instance of InfoNCELoss.
+
+        Parameters
+        ----------
+        sum_dim : int, optional.
+            Dimension to sum over when reducing the similarity matrix.
+            Default is 0.
+        """
+        
         super().__init__()
+
+        self.sum_dim = sum_dim
 
     def forward(self, T_joint: torch.tensor, T_product: torch.tensor) -> torch.tensor:
         """
@@ -30,9 +42,10 @@ class InfoNCELoss(BaseVariationalBoundLoss):
             Critic network value on all samples from the batch.
         T_product : torch.tensor
             Critic network value on all possible pairs of samples from the batch.
+            Rows correspond to anchors.
         """
 
         batch_size = T_joint.shape[0]
         T_product = T_product.view((batch_size, batch_size))
         
-        return torch.mean(torch.logsumexp(T_product - T_joint, dim=-2)) - math.log(batch_size)
+        return torch.mean(torch.logsumexp(T_product, dim=self.sum_dim) - T_joint) - math.log(batch_size)